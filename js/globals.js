7f581ded36726f946bdcf714c9920487c52a4c12
Jakub Brehuv <kubek.breha@gmail.com>
2019-09-04 09:08:36 +0200

    v1.0 sk language

---
+++ js/globals.js
@@ -0,0 +1,27 @@
+
+// On load setting
+var language = "EN";
+
+window.onload = function what() {
+    var cookLang = Cookies.get('language');
+
+    if (cookLang != undefined) {
+        language = cookLang;
+    }
+
+    document.getElementById('languageSwitch').innerHTML = language;
+};
+
+
+// After change setting
+function changeLanguage() {
+    console.log("change language");
+    
+    if (language == "EN") {
+        language = "SK";
+    } else {
+        language = "EN";
+    }
+    document.getElementById('languageSwitch').innerHTML = language;
+    Cookies.set('language', language);
+}
