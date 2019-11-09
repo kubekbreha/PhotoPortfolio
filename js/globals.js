c71f1b3e414536267d3a15c0a33e28129e1244f8
Jakub Brehuv <kubek.breha@gmail.com>
2019-11-09 18:06:39 +0100

    add 4 new albums, wave, malalata, hcke, d&d

--- js/globals.js
+++ js/globals.js
@@ -1,27 +1,27 @@
 
-// On load setting
-var language = "EN";
+// // On load setting
+// var language = "EN";
 
-window.onload = function what() {
-    var cookLang = Cookies.get('language');
+// window.onload = function what() {
+//     var cookLang = Cookies.get('language');
 
-    if (cookLang != undefined) {
-        language = cookLang;
-    }
+//     if (cookLang != undefined) {
+//         language = cookLang;
+//     }
 
-    document.getElementById('languageSwitch').innerHTML = language;
-};
+//     document.getElementById('languageSwitch').innerHTML = language;
+// };
 
 
-// After change setting
-function changeLanguage() {
-    console.log("change language");
+// // After change setting
+// function changeLanguage() {
+//     console.log("change language");
     
-    if (language == "EN") {
-        language = "SK";
-    } else {
-        language = "EN";
-    }
-    document.getElementById('languageSwitch').innerHTML = language;
-    Cookies.set('language', language);
-}
+//     if (language == "EN") {
+//         language = "SK";
+//     } else {
+//         language = "EN";
+//     }
+//     document.getElementById('languageSwitch').innerHTML = language;
+//     Cookies.set('language', language);
+// }
